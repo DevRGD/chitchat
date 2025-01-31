9cf5798d738dd5aec857c3c181034db41a28f9d8
programmergautam <gautm.das@gmail.com>
2025-02-01 01:28:49 +0530

    settings.py/STATIC_URL

--- ChitChat/settings.py
+++ ChitChat/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# For local development:
+# include the static directory in your project
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# For production (on Render or other environments):
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
